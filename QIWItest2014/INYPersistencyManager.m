f848393fc1ce6631c1ddee460bffabe70bf5f347
Nice <nice@iMac-Nice.local>
2014-09-11 20:15:50 +0400

    PersistencyManager

--- QIWItest2014/INYPersistencyManager.m
+++ QIWItest2014/INYPersistencyManager.m
@@ -7,6 +7,7 @@
 //
 
 #import "INYPersistencyManager.h"
+#import "INYUsers.h"
 
 @interface INYPersistencyManager ()
 {
@@ -21,31 +22,11 @@
     self = [super init];
     if (self)
     {
-        albums = [NSMutableArray arrayWithArray:
-                  @[[[Album alloc] initWithTitle:@"Best of Bowie"
-                                          artist:@"David Bowie"
-                                        coverUrl:@"http://www.coversproject.com/static/thumbs/album/album_david%20bowie_best%20of%20bowie.png"
-                                            year:@"1992"],
-                    
-                    [[Album alloc] initWithTitle:@"It's My Life"
-                                          artist:@"No Doubt"
-                                        coverUrl:@"http://www.coversproject.com/static/thumbs/album/album_no%20doubt_its%20my%20life%20%20bathwater.png"
-                                            year:@"2003"],
-                    
-                    [[Album alloc] initWithTitle:@"Nothing Like The Sun"
-                                          artist:@"Sting"
-                                        coverUrl:@"http://www.coversproject.com/static/thumbs/album/album_sting_nothing%20like%20the%20sun.png"
-                                            year:@"1999"],
-                    
-                    [[Album alloc] initWithTitle:@"Staring at the Sun"
-                                          artist:@"U2"
-                                        coverUrl:@"http://www.coversproject.com/static/thumbs/album/album_u2_staring%20at%20the%20sun.png"
-                                            year:@"2000"],
-                    
-                    [[Album alloc] initWithTitle:@"American Pie"
-                                          artist:@"Madonna"
-                                        coverUrl:@"http://www.coversproject.com/static/thumbs/album/album_madonna_american%20pie.png"
-                                            year:@"2000"]]];
+        users = [NSMutableArray arrayWithArray:
+                  @[[[INYUsers alloc] initWithId:@"111" name:@"Vanya" secondName:@"Sedoy"],
+                    [[INYUsers alloc] initWithId:@"222" name:@"Vanya2" secondName:@"Sedoy2"],
+                    [[INYUsers alloc] initWithId:@"333" name:@"Vanya3" secondName:@"Sedoy3"]
+                    ]];
     }
     return self;
 }
